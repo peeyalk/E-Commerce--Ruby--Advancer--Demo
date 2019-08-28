46d8a8ccb14c081d98ee4a9914bb11cea5d4706a
Khondokar Hashibul Islam <khondokar.hash@gmail.com>
2019-08-29 05:58:05 +0900

    Added Style sheets

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,27 @@
 class RegistrationsController < Device::RegistrationsController
 
-  private
 
-  def sign_up_paras
-    param.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
+  protect_from_forgery with: :exception
 
-  def account_update_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # private
+
+  # def sign_up_paras
+  #   param.require(:user).permit(:name, :email, :password, :password_confirmation)
+  # end
+
+  # def account_update_params
+  #   params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
+  # end
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation)}
+
+      devise_parameter_sanitizer.permit(:account_update_params) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password)}
+    end
   end
 
 end
