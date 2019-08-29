75ac8f19737289a236e6d2345013e6a36e95aa69
Khondokar Hashibul Islam <khondokar.hash@gmail.com>
2019-08-29 11:23:24 +0900

    Solved name not saving issue

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,27 +1,19 @@
 class RegistrationsController < Device::RegistrationsController
 
 
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # private
+  private
 
-  # def sign_up_paras
-  #   param.require(:user).permit(:name, :email, :password, :password_confirmation)
-  # end
-
-  # def account_update_params
-  #   params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
-  # end
-
-  protected
-
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation)}
-
-      devise_parameter_sanitizer.permit(:account_update_params) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password)}
-    end
+  def sign_up_params
+    param.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+
+  def account_update_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
+  end
+
 
 end
